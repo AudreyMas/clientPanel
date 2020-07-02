3050572844ce19de84db7b939d21d5f3ea1a779c
Audrey M <masambaaudrey1@gmail.com>
2020-07-02 10:34:12 +0200

    Section 9 58.Creat clientPanel app + bootstrap + components + navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './compornents/navbar/navbar.component';
+import { LoginComponent } from './compornents/login/login.component';
+import { RegisterComponent } from './compornents/register/register.component';
+import { ClientDetailsComponent } from './compornents/client-details/client-details.component';
+import { DashboardComponent } from './compornents/dashboard/dashboard.component';
+import { ClientComponent } from './compornents/client/client.component';
+import { SidebarComponent } from './compornents/sidebar/sidebar.component';
+import { AddClientComponent } from './compornents/add-client/add-client.component';
+import { EditClientComponent } from './compornents/edit-client/edit-client.component';
+import { SettingsComponent } from './compornents/settings/settings.component';
+import { NotFoundComponent } from './compornents/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    ClientDetailsComponent,
+    DashboardComponent,
+    ClientComponent,
+    SidebarComponent,
+    AddClientComponent,
+    EditClientComponent,
+    SettingsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
