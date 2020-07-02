f908c205c535b1d28f30c9aa598ddfd5d60dddc2
Audrey M <masambaaudrey1@gmail.com>
2020-07-02 12:51:45 +0200

    61. client.component = clients... + pipe to client html file to show the client from firebase

--- src/app/compornents/client/client.component.ts
+++ src/app/compornents/client/client.component.ts
@@ -9,11 +9,23 @@ import {Client} from '../../models/Client'
 })
 export class ClientComponent implements OnInit {
   clients: Client[];
+  totalOwed: number;
+
 
   constructor( private clientService: ClientService) { }
 
   ngOnInit(): void {
-    this.clientService.getClients().subscribe(clients => console.log(clients)); // return an Observable so we need to subscribe
+    this.clientService.getClients().subscribe(clients => {
+      this.clients = clients;
+      this.getTotalOwed();
+    
+    }); // return an Observable so we need to subscribe to
+  }
+
+  getTotalOwed(){ //to get all the client
+    this.totalOwed = this.clients.reduce((total, client)=>{
+      return total + client.balance;
+    }, 0);
   }
 
 }
