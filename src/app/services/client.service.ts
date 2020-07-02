df4b970460f8af0ea084d7148a77cd1cc587dcd3
Audrey M <masambaaudrey1@gmail.com>
2020-07-02 23:36:10 +0200

    64. PROBLEM with the visibility of new clients from firebase + clientDteail

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -30,18 +30,30 @@ export class ClientService {
   getClients(): Observable<Client[]> {
     // Get cleints with the id
     this.clients = this.clientsCollection.snapshotChanges().pipe(map(changes => {
-          return changes.map(action => {
-            const data = action.payload.doc.data() as Client;
-            data.id = action.payload.doc.id;
-            return data;
-          });
-        }));
-        return this.clients;
-  }
- 
-  newClient(client: Client){
-    this.clientsCollection.add(client);
+      return changes.map(action => {
+        const data = action.payload.doc.data() as Client;
+        data.id = action.payload.doc.id;
+        return data;
+      });
+    }));
+    return this.clients;
   }
 
+  newClient(client: Client) {
+    this.clientsCollection.add(client);
+  }
+  getClient(id: string): Observable<Client> {
+    this.clientDoc = this.afs.doc<Client>(`clients/${id}`);
+    this.client = this.clientDoc.snapshotChanges().pipe(map(action => {
+      if (action.payload.exists === false) {
+        return null;
+      } else {
+        const data = action.payload.data() as Client;
+        data.id = action.payload.id;
+        return data;
+      }
+    }));
+    return this.client; 
+  }
 
 }
